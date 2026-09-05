785776c4e87d0a2f53ce088a42fd73296fc0115c
llanquilef <llanquilef.cl@gmail.com>
2026-09-05 14:39:15 -0400

    new configuration for sniffer platform

--- sniffer.py
+++ sniffer.py
@@ -1,10 +1,101 @@
 """ SNIFFER """
+from typing import Any
 import sys
 import argparse
 from scapy.all import (
     sniff
     )
 from logger import Logger
+
+
+dict_configuration: dict[str, Any] = {
+    "sniffer": {
+        1: {
+            "command": "-f",
+            "reference": "--filter",
+            "type": str,
+            "choices": ["", "tcp", "udp", "icmp"],
+            "default": "",
+            "help": "Filter for your report: TCP, UDP, ICMP, ("" -> Stands for general)"
+            },
+        2: {
+            "command": "-i",  # ifaces
+            "reference": "--dst",
+            "type": str,
+            "nargs": "+",
+            "help": "How many packets you want to review"
+        },
+        3: {
+            "command": "-c",
+            "reference": "--count",
+            "type": int,
+            "help": "How many packets you want to review" 
+        }
+    },
+    "scanner": {
+        1: {
+            "command": "-ip",
+            "type": str,
+            "help": "IP Address Destiny"
+        },
+        2: {
+            "command": "-p",
+            "reference": "--ports",
+            "type": int,
+            "help": "Ports to review"
+        }
+    },
+    "packet_creator": {
+        1: {
+            "command": "-t",
+            "reference": "--ttl",
+            "type": int,
+            "default": 10,
+            "required": True
+        },
+        2: {
+            "command": "-d",
+            "reference": "--dst",
+            "type": str,
+            "required": True
+        },
+        3: {
+            "command": "-s",
+            "reference": "--src",
+            "type": str,
+            "required": True,
+        },
+        4: {
+            "command": "-f",
+            "reference": "--flags",
+            "choices": ["MF", "evil"],
+            "type": str,
+            "default": None,
+            "required": False
+        }
+    },
+    "syn_scan": {
+        1: {
+            "command": "-i",
+            "reference": "--dst",
+            "type": str,
+            "help": "Direction Port For Scanning"
+        },
+        2: {
+            "command": "-sp",
+            "reference": "--sport",
+            "type": int,
+            "help": "Source Port"
+        },
+        3: {
+            "command": "-dp",
+            "reference": "--dports",
+            "type": int,
+            "nargs": "+",
+            "help": "Direction Port For Scanning"
+        }
+    }
+}
 
 
 class Sniffer():
@@ -24,12 +115,14 @@ class Sniffer():
             description="Sniffer TCP and UDP for specifics IP's"
             )
         try:
-            parser.add_argument("-f", "--filter",
-                                type=str,
-                                choices=["", "udp", "tcp", "icmp", "arp"],
-                                default="",
-                                help="Filter for your report: TCP, UDP, ICMP"
-                                )
+            parser.add_argument(
+                dict_configuration["sniffer"][1]["command"],
+                dict_configuration["sniffer"][1]["reference"],
+                type=dict_configuration["sniffer"][1]["type"],
+                choices=dict_configuration["sniffer"][1]["choices"],
+                default=dict_configuration["sniffer"][1]["default"],
+                help=dict_configuration["sniffer"][1]["help"]
+                )
 
             parser.add_argument("-i", "--ifaces", type=str, nargs="+",
                                 help="Interfaces that you need to scan")
