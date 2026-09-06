d836b9463a63a3d01a74e2e00c4e76568b0a7583
llanquilef <llanquilef.cl@gmail.com>
2026-09-05 20:34:34 -0400

    new configuration for scanner script with global config

---
+++ dict.py
@@ -0,0 +1,96 @@
+from typing import Any
+
+dict_configuration: dict[str, Any] = {
+    "sniffer": {
+        1: {
+            "command": "-f",
+            "reference": "--filter",
+            "type": str,
+            "choices": ["", "tcp", "udp", "icmp"],
+            "default": "",
+            "help": "Filter for your report: TCP, UDP, ICMP, ("" -> Stands for general)"
+            },
+        2: {
+            "command": "-i",  # ifaces
+            "reference": "--dst",
+            "type": str,
+            "nargs": "+",
+            "help": "How many packets you want to review"
+        },
+        3: {
+            "command": "-c",
+            "reference": "--count",
+            "type": int,
+            "help": "How many packets you want to review" 
+        }
+    },
+    "scanner": {
+        1: {
+            "command": "-ip",
+            "type": str,
+            "help": "IP Address Destiny"
+        },
+        2: {
+            "command": "-p",
+            "reference": "--ports",
+            "type": int,
+            "help": "Ports to review"
+        }
+    },
+    "packet_creator": {
+        1: {
+            "command": "-t",
+            "reference": "--ttl",
+            "type": int,
+            "default": 10,
+            "required": True
+        },
+        2: {
+            "command": "-d",
+            "reference": "--dst",
+            "type": str,
+            "required": True
+        },
+        3: {
+            "command": "-s",
+            "reference": "--src",
+            "type": str,
+        },
+        4: {
+            "command": "-f",
+            "reference": "--flags",
+            "choices": ["MF", "evil"],
+            "type": str,
+            "default": None,
+            "required": False
+        }
+    },
+    "syn_scan": {
+        1: {
+            "command": "-i",
+            "reference": "--dst",
+            "type": str,
+            "help": "Direction Port For Scanning"
+        },        
+        2: {
+            "command": "-sp",
+            "reference": "--sport",
+            "type": int,
+            "help": "Source Port"
+        },
+        3: {
+            "command": "-dp",
+            "reference": "--dports",
+            "type": int,
+            "nargs": "+",
+            "help": "Direction Port For Scanning"
+        },
+        4: {
+            "command": "-p",
+            "reference": "--ports",
+            "type": int,
+            "nargs": "+",
+            "help": "Ports to review"
+        }
+    }
+}
