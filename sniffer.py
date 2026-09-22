c4f7e6c598887c2cce1c8194ba79f399ff18582f
llanquilef <llanquilef.cl@gmail.com>
2026-09-21 22:38:38 -0300

    logging for sniffer module

--- sniffer.py
+++ sniffer.py
@@ -1,22 +1,31 @@
 """ SNIFFER """
 import sys
+import logging
 import argparse
 from dict import dict_configuration
 from scapy.all import (
     sniff
     )
-from logger import Logger
 
 
 class Sniffer():
     """ SNIFFER """
     def __init__(self):
-        pass
+        self.logger = logging.getLogger(__class__.__name__)
+        formatter = logging.Formatter("""
+                                      %(asctime)s - %(levelname)s - %(message)s - %(processName)s
+                                      """)
+        handler = logging.StreamHandler()
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+        self.logger.setLevel(logging.INFO)
 
     def sniffer(self, ifaces: str, count: int, filter: str):
         """ SNIFFER """
+        self.logger.info("Sniffing...")
         sniffing = sniff(filter=filter, iface=ifaces,
                          prn=lambda pkt: pkt.show(), count=count)
+        self.logger.info("Finished Sniffing...")
         return sniffing
 
     def parser(self):
@@ -49,26 +58,37 @@ class Sniffer():
 
             args = parser.parse_args()
             return vars(args)
-
         except argparse.ArgumentError as e:
-            print(e)
+            self.logger.error("Parsing Error: %s", e)
             sys.exit(1)
+
+    def store(self):
+        args = self.parser()
+        if args is None:
+            self.logger.warning("")
+        pkts = self.sniffer(ifaces=args.get('ifaces'),
+                            count=args.get('count'),
+                            filter=args.get('filter')
+                            )
+        results = {
+            "tcp_ip": [],
+            "udp_ip": []
+        }
+        self.logger.info("Storing Results...")
+        for pkt in pkts:
+            
+            if "IP" and "TCP" in pkt:
+                results["tcp_ip"].append(pkt)
+                self.logger.info("TCP Packets: %s", results)
+            elif "IP" and "UDP" in pkt:
+                results["udp_ip"].append(pkt)
+                self.logger.info("UDP Packets: %s", results)
 
 
 def main():
     """ MAIN FUNCTION """
-    logger = Logger()
-    logger.get_logger()
-    logger.setLevel()
-    try:
-        sniffer = Sniffer()
-        args = sniffer.parser()
-        sniffer.sniffer(filter=args.get('filter'),
-                        ifaces=args.get('ifaces'),
-                        count=args.get('count'),
-                        )
-    except Exception as e:
-        print(e)
+    sniffer = Sniffer()
+    sniffer.store()
 
 
 if __name__ == "__main__":
