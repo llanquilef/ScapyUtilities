f74a8f2d7b3e761c257fed41d8af546cbf431a82
llanquilef <llanquilef.cl@gmail.com>
2026-09-25 09:40:14 -0300

    Better logging for arguments

--- arp.py
+++ arp.py
@@ -11,34 +11,28 @@ class ARPManager():
         self.broadcast_mac = "ff:ff:ff:ff:ff:ff"
         self.hosts = {}
         self.logger = setup_logger()
-
-    def parser(self) -> argparse.ArgumentParser:
-        parser = argparse.ArgumentParser()
-        parser.add_argument('-i', '--iface', type=str, help="Interfaces to review")
-        parser.add_argument('-r', '--range', type=str, help="IP range")
-        return parser
-
-    def dict_parser(self):
-        args = self.parser()
-        return vars(args.parse_args())
+        self.parser = argparse.ArgumentParser(
+            description=""
+        )
+        self.parser.add_argument('-i', '--iface', type=str, help="Interfaces to review")
+        self.parser.add_argument('-r', '--range', type=str, help="IP range")
+        self.args = vars(self.parser.parse_args())
 
     def arp_packet(self):
         """
         pdst parameter accepts individual addresses
         and CIDR ranges like 10.10.10.0/24
         """
-        args = self.dict_parser()
         ether_layer = Ether(dst=self.broadcast_mac)
-        arp_layer = ARP(pdst=args.get('range'))
+        arp_layer = ARP(pdst=self.args.get('range'))
         return ether_layer / arp_layer
 
     def network_scanner(self, timeout=2):
         try:
             self.logger.info('Starting network scanner...')
-            args = self.dict_parser()
             pkt = self.arp_packet()
             ans, unans = srp(pkt, timeout=timeout,
-                             iface=args.get('iface'),
+                             iface=self.args.get('iface'),
                              inter=0.1, verbose=False
                              )
             for s, r in ans:
